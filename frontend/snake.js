7d6cccd338cf99b2725eacb8c04def46b3edd79b
clintchu <clintchu@outlook.com>
2020-09-06 23:21:22 -0700

    Initialize board

---
+++ frontend/snake.js
@@ -0,0 +1,20 @@
+let width;
+let height;
+let tileSize;
+let canvas;
+let ctx;
+
+// Initialize board and objects
+const board = () => {
+
+    tilesize = 20
+
+    width =tileSize * Math.floow(window.innerWidth / tileSize)
+    height = tileSize * Math.floow(window.innerHeight / tileSize)
+
+    canvas = document.getElementById("game-area")
+    canvas.width = width
+    canvas.height = height
+    ctx = canvas.getContext("2d")
+
+}
