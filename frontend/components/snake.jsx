184b32f791603a2ac779cbf5de9fc8816eccdf3c
clintchu <clintchu@outlook.com>
2020-09-06 23:50:20 -0700

    Setup Snake component

--- frontend/components/snake.jsx
+++ frontend/components/snake.jsx
@@ -1,4 +1,5 @@
 const { createContext } = require("react")
+const { default: food } = require("./food")
 
 const Snake = (pos, color) => {
 
@@ -35,7 +36,52 @@ const Snake = (pos, color) => {
     }
 
     const move = () => {
-        
+
+        for (let i = tail.length - 1; i > 0; i--) {
+            tail[i] = tail[i-1]
+        }
+
+        if (tail.length != 0) 
+            tail[0] = { x: x, y: y }
+
+        x += velX * tileSize
+        y += velY * tileSize
+
+    }
+
+    const dir = (dirX, dirY) => {
+
+        velX = dirX
+        velY = dirY
+
+    }
+
+    const eat = () => {
+
+        if (Math.abs(x - food.x) < tileSize && Math.abs(y - food.y) < tileSize) {
+            tail.push({})
+            return true
+        }
+
+    }
+
+    const die = () => {
+
+        for (let i = 0; i < tail.length; i++) {
+            if (Math.abs(x - tail[i].x) < tileSize && Math.abs(y - tail[i].y) < tileSize) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    const border = () => {
+        if (x + tileSize > width && velX != -1 || x < 0 && velX !=1) {
+            x = width - x
+        } else if (y + tileSize > height && velY != -1 ||  velY ~= 1 && y < 0) {
+            y = height - y
+        }
     }
 
 }
